f3a5ae1a21e32049cd52c2ef692a5e9b96b7ba22
Ram Jonchhen <46254432+Ramjonchhen@users.noreply.github.com>
2020-02-15 17:57:06 +0545

    Heart Beat Animation Done
    Heart Beat Animation Done with text.

--- main.cpp
+++ main.cpp
@@ -1,63 +1,49 @@
-// transformation of the triangale
-// implemented by ram jonchhen
 #include <iostream>
 #include <graphics.h>
-
+#include <stdio.h>
+#include <conio.h>
 using namespace std;
 
-// function to tranform the points
-void trans(int x1,int y1,int x2,int y2,int x3,int y3)
+void clrscr()
 {
-    int scal[3][3];
-    cout<<"Enter the scaling matrix"<<endl;
-    // taking the input of the caling matrix
-    for(int i=0; i<=2; i++)
-    {
-        for (int j=0; j<=2; j++)
-        {
-            cout<<"Enter the value for the scal ["<<i<<"]"<<"["<<j<<"] :";
-            cin>>scal[i][j];
-        }
-    }
-
-    // finding the new points for the transformation
-    int x1new,y1new,x2new,y2new,x3new,y3new;
-    x1new=(scal[0][0]*x1)+(scal[0][1]*y1)+(scal[0][2]);
-    y1new=(scal[1][0]*x1)+(scal[1][1]*y1)+(scal[1][2]);
-
-    x2new=(scal[0][0]*x2)+(scal[0][1]*y2)+(scal[0][2]);
-    y2new=(scal[1][0]*x2)+(scal[1][1]*y2)+(scal[1][2]);
-
-    x3new=(scal[0][0]*x3)+(scal[0][1]*y3)+(scal[0][2]);
-    y3new=(scal[1][0]*x3)+(scal[1][1]*y3)+(scal[1][2]);
-
-    // plotting the points of the new transformed object
-    int points[]={x1new,y1new,x2new,y2new,x3new,y3new,x1new,y1new};
-    drawpoly(4,points);
-    outtextxy(x2new+5,y2new+5,"After Transformation");
+   system("cls");
 }
 
 int main()
 {
-    initwindow(1000,1000,"Transformation");
+    initwindow(800,800,"animation pracitse");
 
-    outtextxy(50,50,"Pefroming transformation of the triangale");//displaying texts
-    outtextxy(80,80,"Implemented By ram jonchhen");
-    int x1,y1,x2,y2,x3,y3;
-    // taking the input points for the triangale
-    cout<<"Enter the points (x1,y1) of the triangale"<<endl;
-    cin>>x1>>y1;
-    cout<<"Enter the points (x2,y2) of the triangale"<<endl;
-    cin>>x2>>y2;
-    cout<<"Enter the points (x3,y3) of the triangale"<<endl;
-    cin>>x3>>y3;
-    int points[]={x1,y1,x2,y2,x3,y3,x1,y1};
-    // drawing the triangale
-    drawpoly(4,points);
-    outtextxy(x2+5,y2+5,"Original");
 
-    // passing the points for the transformation
-    trans(x1,y1,x2,y2,x3,y3);
+    // filiing color
+    //setfillstyle(SOLID_FILL,RED);
+    //floodfill(240,200,WHITE);
+setfillstyle(SOLID_FILL,RED);
+    while(1)
+    {
+
+        for (int i=0; i<=30; i++)
+    {
+        settextstyle(8,0,6);
+        outtextxy(225,75,"I");
+        outtextxy(225,350,"U");
+        delay(10);
+        arc(200-i,200-i,0,180,40+i);
+        arc(280+i,200-i,0,180,40+i);
+        delay(10);
+        line(160-2*i,200-i,240+i,300+2*i);
+        delay(10);
+        line(320+2*i,200-i,240+i,300+2*i);
+    delay(10);
+    delay(10);
+        cleardevice();
+    }
+    }
+
+
+
+
     getch();
+
+
     return 0;
 }
